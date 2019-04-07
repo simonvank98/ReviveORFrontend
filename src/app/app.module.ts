f4269f9426bd1fa00f11538cb85f53051fe940ef
simonvank <nomisvank@hotmail.com>
2019-04-07 12:56:33 +0200

    base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HomeComponent } from './core/home/home.component';
+import { ShopListComponent } from './shop/shop-list/shop-list.component';
+import { ShopItemComponent } from './shop/shop-list/shop-item/shop-item.component';
+import { ShopSearchComponent } from './shop/shop-search/shop-search.component';
+import { ShopDetailComponent } from './shop/shop-detail/shop-detail.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ShopListComponent,
+    ShopItemComponent,
+    ShopSearchComponent,
+    ShopDetailComponent,
+    ShopComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shop/shop-search/shop-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-search',
+  templateUrl: './shop-search.component.html',
+  styleUrls: ['./shop-search.component.css']
+})
+export class ShopSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
