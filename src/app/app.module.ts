138e403c9185510618834f296f9a62d30efceae2
Roy van Dijk <roy.van.dijk@outlook.com>
2019-04-08 21:07:30 +0200

    Revert "Feature/admin panel"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,3 @@
-import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
-import { AdminSideNavigationComponent } from './admin/admin-side-navigation/admin-side-navigation.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -14,47 +12,39 @@ import { ShopSearchComponent } from './shop/shop-search/shop-search.component';
 import { ShopDetailComponent } from './shop/shop-detail/shop-detail.component';
 import { ShopComponent } from './shop/shop.component';
 import { AccountPageComponent } from './account-page/account-page.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TradeInPageComponent } from './trade-in-page/trade-in-page.component';
 import { StoryPageComponent } from './story-page/story-page.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {SharedModule} from './shared/shared.module';
-import { TableComponent } from './shared/table/table.component';
-import { MatTableModule, MatSortModule } from '@angular/material';
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      HeaderComponent,
-      FooterComponent,
-      HomeComponent,
-      ShopListComponent,
-      ShopItemComponent,
-      ShopSearchComponent,
-      ShopDetailComponent,
-      ShopComponent,
-      AccountPageComponent,
-      AdminPanelComponent,
-      ErrorPageComponent,
-      PageNotFoundComponent,
-      TradeInPageComponent,
-      StoryPageComponent,
-      AdminSideNavigationComponent,
-      TableComponent
-   ],
-   imports: [
-      BrowserModule,
-      BrowserAnimationsModule,
-      SharedModule,
-      AppRoutingModule,
-    //   BrowserModule,
-    //   MatTableModule,
-    //   MatSortModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ShopListComponent,
+    ShopItemComponent,
+    ShopSearchComponent,
+    ShopDetailComponent,
+    ShopComponent,
+    AccountPageComponent,
+    AdminPanelComponent,
+    ErrorPageComponent,
+    PageNotFoundComponent,
+    TradeInPageComponent,
+    StoryPageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
