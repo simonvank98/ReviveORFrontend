e940771aa2785a623706cb6b636cfdb9117f09fb
Bruno de Badts <bdebadts@outlook.com>
2019-05-20 15:00:36 +0200

    Added missing attributes to product model

--- src/app/shared/services/product/product.model.ts
+++ src/app/shared/services/product/product.model.ts
@@ -3,14 +3,20 @@ import {ProductCategoryModel} from './product-category.model';
 import {ProductRatingModel} from './product-rating.model';
 
 export interface ProductModel {
-  id: number;
-  name: string;
-  description: string;
-  categoryId: number;
-  status: string;
-  price: number;
-  conditionId: number;
-  images: ProductImageModel[];
-  category: ProductCategoryModel;
-  rating: ProductRatingModel;
+    id: number;
+    name: string;
+    description: string;
+    status: string;
+    price: number;
+    material: string;
+    property: string;
+    conditionId: number;
+    categoryId: number;
+
+    images: ProductImageModel[];
+    category: ProductCategoryModel;
+    rating: ProductRatingModel;
+
+    createdAt?: Date;
+    updatedAt?: Date;
 }
