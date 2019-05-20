31cb4f83abe0c58d7599f1e3bcd44d5579d07df5
MrSanchez <mistersanchezhd@gmail.com>
2019-05-20 21:03:26 +0200

    Added shopping cart feature

--- src/app/features/shop/shop-detail/shop-product-details.component.ts
+++ src/app/features/shop/shop-detail/shop-product-details.component.ts
@@ -1,25 +1,54 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductModel} from '../../../shared/services/product/product.model';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ShoppingCartService} from '../cart/cart.service';
+import {ModalService} from '../../../shared/services/modal-service/modal.service';
+import {CurrencyPipe} from '@angular/common';
+import {SnackbarService} from '../../../shared/services/snackbar/snackbar.service';
 
 @Component({
-  selector: 'app-shop-detail',
-  templateUrl: './shop-product-details.component.html',
-  styleUrls: ['./shop-product-details.component.scss']
+    selector: 'app-shop-detail',
+    templateUrl: './shop-product-details.component.html',
+    styleUrls: ['./shop-product-details.component.scss'],
 })
 export class ShopProductDetailsComponent implements OnInit {
 
-  product: ProductModel;
+    product: ProductModel;
 
-  constructor(private route: ActivatedRoute) { }
+    constructor(private route: ActivatedRoute,
+                private router: Router,
+                private currencyPipe: CurrencyPipe,
+                private cartService: ShoppingCartService,
+                private snackBarService: SnackbarService,
+                private modalService: ModalService) {
+    }
 
-  ngOnInit() {
-      this.route.params
-          .subscribe(
-              () => {
-                  this.product = this.route.snapshot.data.product;
-              }
-          );
-      console.log(this.product);
-  }
+    ngOnInit() {
+        this.product = this.route.snapshot.data.product;
+    }
+
+
+    onAddToShoppingCartButtonClicked() {
+        try {
+            this.cartService.addProductToCart(this.product);
+            this.showAddedToCartDialog();
+        } catch (e) {
+            this.snackBarService.show('This item is already in your shopping cart.');
+        }
+    }
+
+    private showAddedToCartDialog() {
+        const dialog = this.modalService.createDialog('Item has been added to your cart',
+            `${this.product.name} has been added to your shopping cart.
+
+            Your shopping cart total: ${this.currencyPipe.transform(this.cartService.getCartValue(), '€')}`);
+
+        dialog.addButton('Continue shopping', () => {
+        }).buttonColor = 'primary';
+        dialog.addButton('View shopping cart', () => {
+            this.router.navigate(['/shop/cart']);
+        });
+
+        this.modalService.showDialog(dialog);
+    }
 }
--- src/app/features/shop/shop.module.ts
+++ src/app/features/shop/shop.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {SharedModule} from '../../shared/shared.module';
-import {CommonModule} from '@angular/common';
+import {CommonModule, CurrencyPipe} from '@angular/common';
 import {AllProductsResolver} from './all-products.resolver';
 import {ShopProductOverviewComponent} from './shop-list/shop-product-overview.component';
 import {ShopItemComponent} from './shop-list/shop-item/shop-item.component';
@@ -9,6 +9,7 @@ import {ShopProductDetailsComponent} from './shop-detail/shop-product-details.co
 import {ShopComponent} from './shop.component';
 import {ProductResolver} from './product.resolver';
 import {ShoppingCartComponent} from './cart/shopping-cart.component';
+import { ShoppingCartItemComponent } from './cart/shopping-cart-item/shopping-cart-item.component';
 
 @NgModule({
     imports: [
@@ -22,10 +23,12 @@ import {ShoppingCartComponent} from './cart/shopping-cart.component';
         ShopProductDetailsComponent,
         ShopComponent,
         ShoppingCartComponent,
+        ShoppingCartItemComponent,
     ],
     providers: [
         AllProductsResolver,
-        ProductResolver
+        ProductResolver,
+        CurrencyPipe
     ]
 })
 
