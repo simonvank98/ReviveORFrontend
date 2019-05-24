dc1cf21555f41449ca9bad5ad519aedcdee06370
simonvank98 <nomisvank@hotmail.com>
2019-05-24 12:20:09 +0200

    beginning of filtering products

--- src/app/features/shop/shop-list/shop-product-overview.component.ts
+++ src/app/features/shop/shop-list/shop-product-overview.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ProductModel} from '../../../shared/services/product/product.model';
 import {ProductService} from '../../../shared/services/product/product.service';
 import {ActivatedRoute} from '@angular/router';
+import {forEach} from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-shop-list',
@@ -10,15 +11,22 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ShopProductOverviewComponent implements OnInit {
     products: ProductModel[];
+    filteredProducts: ProductModel[];
 
   constructor(private route: ActivatedRoute) { }
 
     ngOnInit() {
         this.products = this.route.snapshot.data['products'];
-        console.log(this.products);
+        this.filteredProducts = this.products;
     }
 
     filterCategory(event) {
-      //
+        event.value.forEach(category => {
+            this.filteredProducts = this.products.filter(product => {
+                console.log(category);
+                return product.category.name.toString().toLocaleLowerCase() === category.toString().toLocaleLowerCase();
+            });
+        });
+        console.log('last one', this.filteredProducts);
     }
 }
