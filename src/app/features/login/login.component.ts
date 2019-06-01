50a216adf8efdfc1cf6dce2ed3f8d6a3b6a0d2ed
Stefan de Keijzer <stefandekeijzer@xs4all.nl>
2019-06-01 20:16:13 +0200

    Added redirect option

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from '../../authentication.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
     selector: 'app-login',
@@ -13,16 +13,25 @@ export class LoginComponent implements OnInit {
     password: string;
     showWarning = false;
     warning = '';
+    private redirect;
 
-    constructor(public authenticationService: AuthenticationService, private router: Router) {
+    constructor(public authenticationService: AuthenticationService, private router: Router, public route: ActivatedRoute) {
     }
 
     ngOnInit() {
+        this.route.queryParams.subscribe(params => {
+            this.redirect = params['redirectTo'];
+        });
     }
 
     onSubmit() {
         this.authenticationService.login(this.email, this.password).subscribe(data => {
-            this.router.navigate(['me', 'edit']);
+            console.log(this.redirect);
+            if (this.redirect) {
+                this.router.navigate(['/', this.redirect]);
+            } else {
+                this.router.navigate(['me', 'edit']);
+            }
         }, error => {
             this.showWarning = true;
             this.warning = 'Username or Password is incorrect';
