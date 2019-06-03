4da64f90c765deee290a047340745dc9227676b9
Stefan de Keijzer <stefandekeijzer@xs4all.nl>
2019-06-03 16:29:29 +0200

    Image upload fix

--- src/app/features/auth/auth.interceptor.ts
+++ src/app/features/auth/auth.interceptor.ts
@@ -18,45 +18,28 @@ export class AuthInterceptor implements HttpInterceptor {
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (!req.headers.has('Content-Type')) {
-            req = req.clone({
-                headers: req.headers.set('Content-Type', 'application/json')
-            });
-        }
-
         req = this.addAuthenticationToken(req);
-
         return next.handle(req).pipe(
             catchError((error: HttpErrorResponse) => {
                 if (error && error.status === 401) {
-                    // 401 errors are most likely going to be because we have an expired token that we need to refresh.
                     if (this.refreshTokenInProgress) {
-                        // If refreshTokenInProgress is true, we will wait until refreshTokenSubject has a non-null value
-                        // which means the new token is ready and we can retry the request again
                         return this.refreshTokenSubject.pipe(
                             filter(result => result !== null),
                             take(1),
                             switchMap(() => next.handle(this.addAuthenticationToken(req)))
                         );
-                    } else {
-                        this.refreshTokenInProgress = true;
-
-                        // Set the refreshTokenSubject to null so that subsequent API calls will wait until the new token has been retrieved
-                        this.refreshTokenSubject.next(null);
-
-                        return this.refreshAccessToken().pipe(
-                            switchMap((success: boolean) => {
-                                this.refreshTokenSubject.next(success);
-                                return next.handle(this.addAuthenticationToken(req));
-                            }),
-                            // When the call to refreshToken completes we reset the refreshTokenInProgress to false
-                            // for the next time the token needs to be refreshed
-                            finalize(() => this.refreshTokenInProgress = false)
-                        );
                     }
-                } else {
-                    return throwError(error);
+                    this.refreshTokenInProgress = true;
+                    this.refreshTokenSubject.next(null);
+                    return this.refreshAccessToken().pipe(
+                        switchMap((success: boolean) => {
+                            this.refreshTokenSubject.next(success);
+                            return next.handle(this.addAuthenticationToken(req));
+                        }),
+                        finalize(() => this.refreshTokenInProgress = false)
+                    );
                 }
+                return throwError(error);
             })
         );
     }
@@ -66,8 +49,6 @@ export class AuthInterceptor implements HttpInterceptor {
     }
 
     private addAuthenticationToken(request: HttpRequest<any>): HttpRequest<any> {
-        // If we do not have a token yet then we should not set the header.
-        // Here we could first retrieve the token from where we store it.
         if (!this.authenticationService.token) {
             return request;
         }
