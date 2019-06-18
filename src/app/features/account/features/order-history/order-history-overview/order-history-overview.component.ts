62396bd21310e3828df97a957d869307505c8f98
simonvank98 <nomisvank@hotmail.com>
2019-06-18 16:55:06 +0200

    beginning of order-history

--- src/app/features/account/features/order-history/order-history-overview/order-history-overview.component.ts
+++ src/app/features/account/features/order-history/order-history-overview/order-history-overview.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-order-history-overview',
@@ -6,10 +7,16 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./order-history-overview.component.scss']
 })
 export class OrderHistoryOverviewComponent implements OnInit {
+    models = [];
+    headers = ['Status', 'Jewelry', 'Estimated credit', 'Request date', 'Last update'];
+    attributes = ['status', 'jewelryName', 'estimatedCredit', 'createdAt', 'updatedAt'];
+    formatters = [(attr) => attr, (attr) => attr, (attr) => attr, (createdAt) => new Date(createdAt).toLocaleDateString(),
+        (updatedAt) => new Date(updatedAt).toLocaleDateString()];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+      // this.models = this.route.snapshot.data['requests'];
   }
 
 }
